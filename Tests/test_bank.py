96551279feaec3aa8bcfa90f2eabaea469b45718
Abhi Katta <abhinaykatta97@gmail.com>
2023-09-24 16:46:08 +0530

    pylint code readability test

--- Tests/test_bank.py
+++ Tests/test_bank.py
@@ -16,12 +16,22 @@ def bank():
 def test_proper_work(bank):
     bank.create_account(accountID='acc1', initial_amount=100)
     assert bank.get_balance('acc1') == 100
+    bank.withdraw(accountID='acc1', withdraw_money=50)
+    assert bank.get_balance('acc1') == 50
+    bank.add_to_savings(accountID='acc1', debit_money=200)
+    assert bank.get_balance('acc1') == 250
 
 
 def test_duplicacy(bank):
     bank.create_account(accountID='acc2', initial_amount=202)
     with pytest.raises(ValueError):
         assert bank.create_account(accountID='acc2', initial_amount=202)
+
+
+def test_duplicacy2(bank):
+    bank.create_account(accountID='acc3', initial_amount=202)
+    with pytest.raises(ValueError):
+        assert bank.create_account(accountID='acc5', initial_amount=202)
 
 
 def test_negative_deposit(bank):
--- src/Bank/bank.py
+++ src/Bank/bank.py
@@ -1,42 +1,60 @@
+'''Some docstring about this Bank class'''
+
+
 class Bank:
+    '''Bank with operations for creating account, withdrawing amount,
+        adding amount to savings, getting remaining balance. '''
+
     def __init__(self) -> None:
         self.accounts = {}
 
-    def create_account(self, accountID: str, initial_amount: int or float):
-        if accountID in self.accounts:
-            raise ValueError(
-                'Account ID already in use!')
-        if initial_amount > 0:
-            self.accounts[accountID] = initial_amount
-        else:
-            raise ValueError(
-                'Insufficient deposit. Deposit more than 2$ for creating an account')
+    def create_account(self, account_id: str, initial_amount: int or float) -> None:
+        '''
+        Creates a new bank account with account_id and initial_amount
+        account_id: ID for the bank account.
+        initial_amount: Funds to be deposited into the account when the account is created.
+        returns None
 
-    def add_to_savings(self, accountID: str, debit_money: int or float):
-        if accountID not in self.accounts:
-            raise ValueError(f'No account found with account ID:{accountID}')
-        if debit_money > 0:
-            self.accounts[accountID] += debit_money
-        else:
+        '''
+        if account_id in self.accounts:
+            raise ValueError('Account ID already in use!')
+        if initial_amount < 0:
             raise ValueError(
-                'Insufficient deposit. Deposit more than 1$ for debiting money into the account')
+                'Insufficient deposit.\
+                Deposit more than 2$ for creating an account')
+        self.accounts[account_id] = initial_amount
 
-    def withdraw(self, accountID: str, withdraw_money: int or float):
-        if accountID not in self.accounts:
-            raise ValueError(f'No account found with account ID:{accountID}')
-        if withdraw_money > self.accounts[accountID]:
-            raise ValueError(
-                f'Insufficient deposit. Credit amount exceeding existing balance of {self.accounts[accountID]}$')
-        elif withdraw_money < 0:
-            raise ValueError(
-                f'Incorrect Withdraw amount of {withdraw_money}$.')
+    def add_to_savings(self, account_id: str, amount: int or float):
+        '''
+        Adds amount to the account of account with id account_id.
+        '''
+        if account_id not in self.accounts:
+            raise ValueError(f'No account found with account ID:{account_id}')
+        if amount > 0:
+            self.accounts[account_id] += amount
         else:
-            self.accounts[accountID] -= withdraw_money
-            print(
-                f"Withdrawn {withdraw_money}$. Remaining balance :{self.accounts[accountID]}$.")
+            raise ValueError(
+                'Insufficient deposit.\
+                Deposit more than 1$ for debiting money into the account')
 
-    def get_balance(self, accountID):
-        if accountID in self.accounts:
-            return self.accounts[accountID]
-        else:
-            raise ValueError("Invalid account ID.")
+    def withdraw(self, account_id: str, amount: int or float):
+        '''Withdraw money from the bank of account passed as account_id.'''
+        if account_id not in self.accounts:
+            raise ValueError(f'No account found with account ID:{account_id}')
+        if amount > self.accounts[account_id]:
+            raise ValueError(
+                f'Insufficient deposit.\
+                Credit amount exceeding existing balance of {self.accounts[account_id]}$')
+        if amount < 0:
+            raise ValueError(
+                f'Incorrect Withdraw amount of {amount}$.')
+        self.accounts[account_id] -= amount
+        print(
+            f"Withdrawn {amount}$. \
+            Remaining balance :{self.accounts[account_id]}$.")
+
+    def get_balance(self, account_id):
+        '''Print out the remaining balance in the bank account with account_id'''
+        if account_id in self.accounts:
+            return self.accounts[account_id]
+        raise ValueError("Invalid account ID.")
